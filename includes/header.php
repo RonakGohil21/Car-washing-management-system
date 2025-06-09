1bf6e68766c54274ca5882f88dabaa67b102e1aa
RonakGohil21 <rngohil2144@gmail.com>
2025-06-09 12:45:51 +0530

    includes , js and libraries

---
+++ includes/header.php
@@ -0,0 +1,48 @@
+        <!-- Top Bar Start -->
+        <div class="top-bar" style="background-color:rgb(58, 88, 118);;">
+            <div class="container" >
+                <div class="row align-items-center" >
+                    <div class="col-lg-4 col-md-12" style="display:flex; margin:10px; ">
+                        <div class="logo" >                            
+                            <a href="index.php">
+                            <h1><img src="img/main.webp" style="height: 45px; width: 45px;" alt="Logo"></h1>
+                            </a>
+                        </div>
+                        <div class="container">
+                        <a href="index.php">
+                       <h3 style="color:rgb(171, 195, 246); font-weight: 700; margin-top:9px;" >Z+ CAR <span>WASH</span></h3> 
+                        </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Top Bar End -->
+
+        <!-- Nav Bar Start -->
+        <div class="nav-bar">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+                    <a href="#" class="navbar-brand">MENU</a>
+                    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                        <div class="navbar-nav mr-auto">
+                            <a href="index.php" class="nav-item nav-link ">Home</a>
+                            <a href="about.php" class="nav-item nav-link">About</a>
+                            <a href="washing-plans.php" class="nav-item nav-link">Washing Plans</a>
+                            <a href="location.php" class="nav-item nav-link">Washing Points</a>
+                            <a href="contact.php" class="nav-item nav-link">Contact</a>
+                            <a href="t&c.php" class="nav-item nav-link">Terms & Conditions</a>
+                            <!-- <a href="admin" class="nav-item nav-link">admin</a> -->
+                        </div>
+                        <div class="ml-auto">
+                            <a class="btn btn-custom" style="background-color:rgb(157, 182, 236);" href="admin">Admin</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <!-- Nav Bar End -->
